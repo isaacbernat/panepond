011c2ef815ba1cf414ce0e2ca5504f7ce7d47fda
isaacbernat <bernat@wrapp.com>
2014-12-14 18:25:34 +0100

    swap tiles

--- web/board.dart
+++ web/board.dart
@@ -41,7 +41,7 @@ class Board extends PolymerElement {
     Controls.down: KeyCode.K,
     Controls.left: KeyCode.J,
     Controls.right: KeyCode.L,
-    Controls.action: KeyCode.CTRL,
+    Controls.action: KeyCode.P,
   };
   @observable  Map<String, num> cursor = toObservable({
     "x": 3,
@@ -60,17 +60,38 @@ class Board extends PolymerElement {
       return;
     }
     if (keyCode ==  controls[Controls.up]) {
-      cursor["y"] = max(0, --cursor["y"]);
+      cursor["y"] = max(0, cursor["y"] -1);
     } else if (keyCode == controls[Controls.down]) {
-      cursor["y"] = min(height -cursor["height"], ++cursor["y"]);
+      cursor["y"] = min(height -cursor["height"], cursor["y"] +1);
     } else if (keyCode == controls[Controls.left]) {
-      cursor["x"] = max(0, --cursor["x"]);
+      cursor["x"] = max(0, cursor["x"] -1);
     } else if (keyCode == controls[Controls.right]) {
-      cursor["x"] = min(width -cursor["width"], ++cursor["x"]);
+      cursor["x"] = min(width -cursor["width"], cursor["x"] +1);
+    }
+    else if (keyCode == controls[Controls.action]) {
+      String x1 = cursor["x"].toString();
+      String x2 = (cursor["x"] +1).toString();
+      String y = cursor["y"].toString();
+      swapTiles(x1, y, x2, y);
     }
   }
-}
 
+  void swapTiles(String x1, String y1, String x2, String y2) {
+    var tile1 =  this.shadowRoot.querySelector('.board.ti [pos="' + x1 + ',' + y1 + '"]');
+    var tile2 =  this.shadowRoot.querySelector('.board.ti [pos="' + x2 + ',' + y2 + '"]');
+    var aux1 = new DivElement(); //TODO: find a better way to deep-copy
+    aux1.appendHtml(tile1.innerHtml);
+    aux1.attributes = tile1.attributes;
+    aux1.attributes["pos"] = tile2.attributes["pos"]; //TODO: use XPath instead of this hack
+    var aux2 = new DivElement();
+    aux2.appendHtml(tile2.innerHtml);
+    aux2.attributes = tile2.attributes;
+    aux2.attributes["pos"] = tile1.attributes["pos"];
+
+    tile1.replaceWith(aux2);
+    tile2.replaceWith(aux1);
+  }
+}
 
 class Column {
   static var rand = new Random();
